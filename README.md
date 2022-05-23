2d7754c762d77d8dc85d262c3cd9ca3d8d048fb8
asachiyigor <asachiyigor@gmail.com>
2022-05-23 19:57:34 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# PatternsTest [![Build status](https://ci.appveyor.com/api/projects/status/dy24g35xx5vw13tv?svg=true)](https://ci.appveyor.com/project/asachiyigor/aqa-patterns-2)
+
+* Время, затраченное на ручное тестирование (минут): 30
+* Время, затраченное на автоматизацию (минут): 60
---
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -0,0 +1,72 @@
+package ru.netology.test;
+
+import com.codeborne.selenide.Condition;
+import lombok.val;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.Data;
+import ru.netology.page.DashboardPage;
+import ru.netology.page.LoginPage;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MoneyTransferTest {
+
+    @BeforeEach
+    void auth() {
+        open("http://localhost:9999", LoginPage.class);
+        val loginPage = new LoginPage();
+        val authInfo = Data.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = Data.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    void shouldTransferMoneyFromFirstCardToSecond() {
+        int amount = 5000;
+        int FirstCardBalance = DashboardPage.getFirstCardBalance();
+        int SecondCardBalance = DashboardPage.getSecondCardBalance();
+        var TransferPage = DashboardPage.secondCard();
+        var cardInfo = Data.getFirstCardInfo();
+        TransferPage.transferCard(cardInfo, amount);
+        int FirstCardAfterTransferBalance = Data.decreaseBalance(FirstCardBalance, amount);
+        int SecondCardAfterTransferBalance = Data.increaseBalance(SecondCardBalance, amount);
+        int FirstCardBalanceAfter = DashboardPage.getFirstCardBalance();
+        int SecondCardBalanceAfter = DashboardPage.getSecondCardBalance();
+        assertEquals(FirstCardAfterTransferBalance, FirstCardBalanceAfter);
+        assertEquals(SecondCardAfterTransferBalance, SecondCardBalanceAfter);
+    }
+
+    @Test
+    void shouldTransferMoneyFromSecondCardToFirst() {
+        int amount = 5000;
+        int FirstCardBalance = DashboardPage.getFirstCardBalance();
+        int SecondCardBalance = DashboardPage.getSecondCardBalance();
+        var TransferPage = DashboardPage.firstCard();
+        var cardInfo = Data.getSecondCardInfo();
+        TransferPage.transferCard(cardInfo, amount);
+        int FirstCardAfterTransferBalance = Data.increaseBalance(FirstCardBalance, amount);
+        int SecondCardAfterTransferBalance = Data.decreaseBalance(SecondCardBalance, amount);
+        int FirstCardBalanceAfter = DashboardPage.getFirstCardBalance();
+        int SecondCardBalanceAfter = DashboardPage.getSecondCardBalance();
+        assertEquals(FirstCardAfterTransferBalance, FirstCardBalanceAfter);
+        assertEquals(SecondCardAfterTransferBalance, SecondCardBalanceAfter);
+    }
+
+    @Test
+    void shouldNotTransferMoneyAndShowErrorWhenAmountIsMoreThanAvailableBalance() {
+        int amount = 20000;
+        int FirstCardBalance = DashboardPage.getFirstCardBalance();
+        int SecondCardBalance = DashboardPage.getSecondCardBalance();
+        var TransferPage = DashboardPage.firstCard();
+        var cardInfo = Data.getSecondCardInfo();
+        TransferPage.transferCard(cardInfo, amount);
+        Data.increaseBalance(FirstCardBalance, amount);
+        Data.decreaseBalance(SecondCardBalance, amount);
+        TransferPage.transferMoneyError();
+    }
+}
